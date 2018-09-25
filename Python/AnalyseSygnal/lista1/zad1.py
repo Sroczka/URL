0b8be121e52b2e7e3e2fe2346e11899db667488e
Sroczka <martajnk@gmail.com>
2018-09-25 11:57:58 +0200

    init

--- Python/AnalyseSygnal/lista1/zad1.py
+++ Python/AnalyseSygnal/lista1/zad1.py
@@ -44,11 +44,110 @@ for i in range (44000):
 #f
 
 
-echantillon2 = []
+echantillon_la = []
 for i in range (44000):
-    echantillon2.append(u(interval[i]))
+    echantillon_la.append(u(interval[i]))
 #g
 fn = open('la.pcm','wb')
 for i in range(44000):
-    fn.write(echantillon2[i])
+    fn.write(echantillon_la[i])
 fn.close()
+
+
+#h 
+#command
+# cvlc --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=44000
+#--rawaud-fourcc "u8  " la.pcm
+
+#i
+
+def do(t):
+    do = round(sin(220*2**(1/4)*2*pi*t))
+    return (do).to_bytes(1,byteorder='big',signed = True)
+
+def re(t):
+    re = round(sin(220*2**(5/12)*2*pi*t))
+    return (re).to_bytes(1,byteorder='big',signed = True)
+
+def mi(t):
+    mi = round(sin(220*2**(7/12)*2*pi*t))
+    return (mi).to_bytes(1,byteorder='big',signed = True)
+
+def fa(t):
+    fa = round(sin(220*2**(2/3)*2*pi*t))
+    return (fa).to_bytes(1,byteorder='big',signed = True)
+
+def sol(t):
+    sol = round(sin(220*2**(5/6)*2*pi*t))
+    return (sol).to_bytes(1,byteorder='big',signed = True)
+
+def si(t):
+    si = round(sin(440*2**(1/6)*2*pi*t))
+    return (si).to_bytes(1,byteorder='big',signed = True)
+
+def do2(t):
+    do = round(sin(440*2**(1/4)*2*pi*t))
+    return (do).to_bytes(1,byteorder='big',signed = True)
+
+echantillon_do = []
+for i in range (44000):
+    echantillon_do.append(do(interval[i]))
+    
+echantillon_re = []
+for i in range (44000):
+    echantillon_re.append(re(interval[i]))
+    
+echantillon_mi = []
+for i in range (44000):
+    echantillon_mi.append(mi(interval[i]))
+    
+echantillon_fa = []
+for i in range (44000):
+    echantillon_fa.append(fa(interval[i]))
+    
+echantillon_sol = []
+for i in range (44000):
+    echantillon_sol.append(sol(interval[i]))
+
+echantillon_si = []
+for i in range (44000):
+    echantillon_si.append(si(interval[i]))
+
+echantillon_do2 = []
+for i in range (44000):
+    echantillon_do2.append(do2(interval[i]))
+
+fdo = open('do.pcm','wb')
+for i in range(44000):
+    fdo.write(echantillon_do[i])
+fdo.close()
+
+fre = open('re.pcm','wb')
+for i in range(44000):
+    fre.write(echantillon_re[i])
+fre.close()
+
+fmi = open('mi.pcm','wb')
+for i in range(44000):
+    fmi.write(echantillon_mi[i])
+fmi.close()
+
+ffa = open('fa.pcm','wb')
+for i in range(44000):
+    ffa.write(echantillon_fa[i])
+ffa.close()
+
+fsol = open('sol.pcm','wb')
+for i in range(44000):
+    fsol.write(echantillon_sol[i])
+fsol.close()
+
+fsi = open('si.pcm','wb')
+for i in range(44000):
+    fsi.write(echantillon_si[i])
+fsi.close()
+
+fdo2 = open('do2.pcm','wb')
+for i in range(44000):
+    fdo2.write(echantillon_do2[i])
+fdo2.close()
