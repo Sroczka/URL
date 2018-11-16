ec45f56d67484a81baf152a1ae5228fe2cb30508
Sroczka <martajnk@gmail.com>
2018-11-16 16:45:45 +0100

    init

--- Python/programmation_objet/wyklad/iterator_16.py
+++ Python/programmation_objet/wyklad/iterator_16.py
@@ -9,61 +9,30 @@ Created on Fri Nov 16 15:06:14 2018
 #szuka wszystkich wielokrotnosci
 
 class A:
-    def __init__(self,premier):
-        self.premier = premier
-        self.etat = 0
+    def __init__(self,liste):
+        self.liste = liste
         
     def __iter__(self):
         return B(self)
-    
-    def multiple(self):
-        self.etat += 1
-        if( self.etat<100):
-            if (self.etat%self.premier == 0): return self.etat
-            else: return None
-        else: raise StopIteration("fin d'itération")
         
 class B:
     def __init__(self,a):
         self.a = a
-    def __next__(self):
-        return a.multiple()
-    
-a = A(29)
-
-for i in a:
-    if i is not None: #jak inaczej ominac wyswietlanie None
-        print(i)
-        
-############inaczej
-#mozna z while, try i except
-        
-#szuka wielokrotosci w liscie        
-        
-class Multiple:
-    def __init__(self,l):
-        self.l = l
-        
-    def __iter__(self):
-        return It(self)
-    
-class It:
-    def __init__(self,m):
-        self.m = m
         self.i = -1
         
     def __next__(self):
         try:
             while True:
                 self.i += 1
-                if self.m.l[self.i] % self.m.l[0] == 0:
-                    return(self.m.l[self.i])
-                    
+                if (self.a.liste[self.i]%self.a.liste[0] == 0):
+                    return (self.a.liste[self.i])
+            
         except IndexError:
             raise StopIteration("fin d'itération")
+    
             
-m = Multiple([2,3,4,5,6,7])
+a = Multiple([2,3,4,5,6,7])
 
-for i in m:
-    for j in m:
+for i in a:
+    for j in a:
         print(f"{i} {j}")
