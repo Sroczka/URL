6d40e3b5cf934458cf465e23e8902c02a2fefd05
Sroczka <martajnk@gmail.com>
2018-11-16 16:11:15 +0100

    init

--- Python/programmation_objet/wyklad/iterator_16.py
+++ Python/programmation_objet/wyklad/iterator_16.py
@@ -6,18 +6,21 @@ Created on Fri Nov 16 15:06:14 2018
 @author: janik
 """
 
+#szuka wszystkich wielokrotnosci
+
 class A:
     def __init__(self,premier):
         self.premier = premier
-        self.etat = premier
+        self.etat = 0
         
     def __iter__(self):
         return B(self)
     
     def multiple(self):
         self.etat += 1
-        if (self.etat%self.premier == 0 and self.etat<50): return self.etat
-        if ((self.etat%self.premier == 1 or self.etat%self.premier == 2) and self.etat<50): return None
+        if( self.etat<100):
+            if (self.etat%self.premier == 0): return self.etat
+            else: return None
         else: raise StopIteration("fin d'itération")
         
 class B:
@@ -26,7 +29,41 @@ class B:
     def __next__(self):
         return a.multiple()
     
-a = A(3)
+a = A(29)
 
 for i in a:
-    print(i)
+    if i is not None: #jak inaczej ominac wyswietlanie None
+        print(i)
+        
+############inaczej
+#mozna z while, try i except
+        
+#szuka wielokrotosci w liscie        
+        
+class Multiple:
+    def __init__(self,l):
+        self.l = l
+        
+    def __iter__(self):
+        return It(self)
+    
+class It:
+    def __init__(self,m):
+        self.m = m
+        self.i = -1
+        
+    def __next__(self):
+        try:
+            while True:
+                self.i += 1
+                if self.m.l[self.i] % self.m.l[0] == 0:
+                    return(self.m.l[self.i])
+                    
+        except IndexError:
+            raise StopIteration("fin d'itération")
+            
+m = Multiple([2,3,4,5,6,7])
+
+for i in m:
+    for j in m:
+        print(f"{i} {j}")
