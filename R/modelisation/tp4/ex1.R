2574bb5bf79402ad3a5b64b69c5d84f8a875a014
Sroczka <martajnk@gmail.com>
2018-12-13 15:08:25 +0100

    init

---
+++ R/modelisation/tp4/ex1.R
@@ -0,0 +1,45 @@
+MMC1 <- function(n){
+  s <- 0
+  for(i in range(1:n)){
+    U <- runif(1)
+    #s = s + exp(U)
+    s = s+ sqrt(1-U^2)
+  }
+  return(s/n)
+}
+
+###################################
+
+MMC2 <- function(n){
+  s <- 0;S = c()
+  for(i in(1:n)){
+    U <- runif(1)
+    #s = s + exp(U)
+    s = s+ sqrt(1-U^2);S = c(S,s)
+  }
+  return(S/1:length(S))
+}
+
+eval <- MMC2(1000)
+plot(1:length(eval),eval, 'l')
+
+#############
+
+MMC_var <- function(n){
+  srednia=0
+  S=c()
+  moment2=0
+  for(i in (1:n)){
+    U = runif(1)
+    srednia = srednia + sqrt(1-U^2)
+    S=c(S,srednia)
+    moment2=moment2+(1-U^2)
+  }
+  return(list(evol=S/1:length(S),varempir= moment2/n-(srednia/n)^2))
+}
+
+evolVar = MMC_var(1000)
+
+evolVar$varempir
+
+##################
